f0b3213f3d97e6a114b52bdea008c0725a47da3a
CLancet <2385366128@qq.com>
2023-12-30 13:31:48 +0800

    feat:整合了neo4j的连接，更新了pom和yaml配置文件

---
+++ src/main/java/com/example/DW_backend/controller/neo4j/MovieController.java
@@ -0,0 +1,26 @@
+package com.example.DW_backend.controller.neo4j;
+import org.neo4j.driver.Driver;
+import org.neo4j.driver.Session;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+@RestController
+public class MovieController {
+
+    private final Driver driver;
+
+    public MovieController(Driver driver) {
+        this.driver = driver;
+    }
+    @GetMapping(path = "/movies", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<String> getMovieTitles() {
+        try (Session session = driver.session()) {
+            return session.run("MATCH (n:Movie) RETURN n LIMIT 25")
+                    .list(r -> r.get("n").asNode().get("name").asString());
+        }
+    }
+
+}
+
