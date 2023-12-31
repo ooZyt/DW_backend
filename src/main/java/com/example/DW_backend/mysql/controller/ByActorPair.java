ce5d69ff0506d27a75b20fbe181758d424838f45
ooZyt <2469882837@qq.com>
2023-12-31 12:12:32 +0800

    feat:修改关系查询接口跨域问题
    feat:修改关系查询接口跨域问题

--- src/main/java/com/example/DW_backend/mysql/controller/ByActorPair.java
+++ src/main/java/com/example/DW_backend/mysql/controller/ByActorPair.java
@@ -1,17 +1,19 @@
 package com.example.DW_backend.mysql.controller;
 
-import com.example.DW_backend.mysql.mapper.ActorMapper;
+
 import com.example.DW_backend.mysql.mapper.ActorPairMapper;
 import com.example.DW_backend.mysql.pojo.ActorPairDTO;
 import com.example.DW_backend.mysql.pojo.MovieDTO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import java.util.List;
 
+@CrossOrigin(origins = {"http://localhost:5173"})
 @RestController
 @RequestMapping("mysql/ByActorPair")
 public class ByActorPair {
@@ -19,9 +21,11 @@ public class ByActorPair {
     @Autowired
     private ActorPairMapper actorPairMapper;
 
-    @GetMapping("")
+    @RequestMapping(value="",method=RequestMethod.GET)
     List<ActorPairDTO> findActorPairsWithMostCollaborations(){
-        return actorPairMapper.findActorPairsWithMostCollaborations();
+        List<ActorPairDTO> AApair=actorPairMapper.findActorPairsWithMostCollaborations();
+       
+        return AApair;
     }
 
 
